177f406e983969f2f8adccf1ea2db0fe845a48e5
Firstname Lastname <matheus.vex@gmail.com>
2024-06-18 19:24:29 -0300

    chore: user crud create

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreateUserInput } from './dtos/create-user-input.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  public async create(data: CreateUserInput) {
+    const email = data.email;
+    const user = this.prisma.user.findUnique({
+      where: { email },
+    });
+    console.log(user);
+    if (user) {
+      throw new Error('Email ja cadastrado');
+    }
+    await this.prisma.user.create({
+      data,
+    });
+  }
+  //   public async findAll() {}
+  //   public async findOne(id) {}
+  //   public async update(id, updateUserDto) {}
+  //   public async remove(id) {}
+}
